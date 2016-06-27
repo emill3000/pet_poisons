780a70f2d03081c0bd2ad23b7d92467fb32a26b9
emill3000 <emill3000@yahoo.com>
2016-06-26 20:54:18 -0700

    scraper work

--- lib/pet_poisons/Poison_Scraper.rb
+++ lib/pet_poisons/Poison_Scraper.rb
@@ -4,10 +4,18 @@ require "pry"
 
 class Poison_Scraper
 
-  def toxic?
+  def poison_list
   	doc = Nokogiri::HTML(open("http://www.petpoisonhelpline.com/poisons/"))
-  	binding.pry
+  	doc.css("ul.landingList a")
+  end
 
+  def toxic?(input)
+  	input = User.input
+  	if poison_list.include input
+  		puts "#{input} is toxic. Would you like more information (y/n)?"
+  	else
+  		puts "#{input} is not toxic. Please call your veterinarian if you have any other concerns."
+  	end
   end
 
 end
